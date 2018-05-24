d249ce2db11db20b30bd33dfddf0b769803967e7
Oleg Bondar <bullet-tooth@users.noreply.github.com>
2018-05-24 17:26:54 +0300

    Readme + test

---
+++ README.md
@@ -0,0 +1,22 @@
+#Spring Reactive Data Mongo `Class` serialization example
+This repo provides information how to store entities with `Class<T>` fields in MongoDB.
+By default Mongo provider does't support such field types.
+
+##To implement `Class<T>` serialization you should:
+* Implement bson `Codec`
+* Implement bson `CodecProvider`
+* Register them to `MongoSettings`
+* Create custom spring data `Converter`
+* Register it to spring context
+
+_Please refer to `com.example.mongoclasspertist.MongoConfiguration` for implementation details._
+
+
+##Technology stack:
+* Spring boot 2.0.2
+* Spring webflux
+* Spring data mongo reactive
+* Embedded mongo (for tests)
+* Lombok
+
+`com.example.mongoclasspertist.ServiceRepositoryTest` provides client calls example.
--- src/test/java/com/example/mongoclasspertist/ServiceRepositoryTest.java
+++ src/test/java/com/example/mongoclasspertist/ServiceRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.example.mongoclasspertist;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.test.StepVerifier;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,19 +20,28 @@ public class ServiceRepositoryTest {
   private ServiceRepository repository;
 
   @Test
-  public void test() {
+  public void testSaveAndFetch() {
     ServiceEntity entity = new ServiceEntity();
     entity.setName("service1");
     entity.setInputClass(String.class);
 
-    repository.save(entity)
-        .doOnNext(e -> log.info("Saved entity: {}", e))
-    .block();
+    StepVerifier.create(repository.save(entity))
+        .expectSubscription()
+        .assertNext(savedEntity -> {
+          assertNotNull(savedEntity.getId());
+          assertEquals(entity.getName(), savedEntity.getName());
+          assertEquals(entity.getInputClass(), savedEntity.getInputClass());
+        })
+        .verifyComplete();
 
-    repository.findAll()
-        .collectList()
-        .doOnNext(all -> log.info("All entities from DB: {}", all))
-        .block();
+    StepVerifier.create(repository.findAll())
+        .expectSubscription()
+        .assertNext(dbEntity -> {
+          assertNotNull(dbEntity.getId());
+          assertEquals(entity.getName(), dbEntity.getName());
+          assertEquals(entity.getInputClass(), dbEntity.getInputClass());
+        })
+        .verifyComplete();
   }
 
 }
